73fdf2bbdd01d5d40e51f21243f184c9316b73b5
Eric Stern <eric@ericstern.com>
2016-02-22 14:17:48 -0800

    Add RegisterRequest object and test

---
+++ src/RegisterRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Firehed\U2F;
+
+use JsonSerializable;
+
+class RegisterRequest implements JsonSerializable
+{
+    use AppIdTrait;
+    use ChallengeTrait;
+    use VersionTrait;
+
+    public function jsonSerialize() {
+        return [
+            "version" => $this->version,
+            "challenge" => $this->getChallenge(),
+            "appId" => $this->getAppId(),
+        ];
+    }
+}
