eb7fd4686616460fb34ed27ad592672504bc7355
Eric Stern <eric@ericstern.com>
2019-05-12 18:08:40 -0700

    Correct some tests

--- tests/CBOR/DecoderTest.php
+++ tests/CBOR/DecoderTest.php
@@ -16,22 +16,21 @@ class DecoderTest extends \PHPUnit\Framework\TestCase
     /**
      * @covers ::decode
      * @dataProvider githubTestVectors
-     */
-    public function testDecodeGH(string $cbor, $decoded)
-    {
-        $decoder = new Decoder();
-        $result = $decoder->decode($cbor);
-        $this->assertSame($decoded, $result);
-    }
-    /**
-     * @covers ::decode
+     * @dataProvider rfcBigintExamples
      * @dataProvider rfcExamples
+     * @dataProvider rfcTagExamples
      */
-    public function testDecodeRFC(string $cbor, $decoded)
+    public function testDecode(string $cbor, $expected)
     {
         $decoder = new Decoder();
         $result = $decoder->decode($cbor);
-        $this->assertSame($decoded, $result);
+        if (is_float($expected) && ($expected > \PHP_INT_MAX) || ($expected < \PHP_INT_MIN)) {
+            $this->assertEquals($expected, $result);
+        } elseif (is_float($expected) && is_nan($expected)) {
+            $this->assertTrue(is_nan($result), 'Result should be NAN');
+        } else {
+            $this->assertSame($expected, $result);
+        }
     }
 
     /**
@@ -40,7 +39,7 @@ class DecoderTest extends \PHPUnit\Framework\TestCase
     public function githubTestVectors(): array
     {
         $json = file_get_contents(__DIR__ . '/vectors.json');
-        $data = json_decode($json, true);
+        $data = json_decode($json, true, 512, JSON_THROW_ON_ERROR | JSON_BIGINT_AS_STRING);
         return array_map(function ($row) {
             return [base64_decode($row['cbor']), $row['decoded']];
         }, array_filter($data, function ($row) {
@@ -66,10 +65,6 @@ class DecoderTest extends \PHPUnit\Framework\TestCase
             [1000, '1903e8'],
             [1000000, '1a000f4240'],
             [1000000000000, '1b000000e8d4a51000'],
-            [18446744073709551615, '1bffffffffffffffff'],
-            [18446744073709551616, 'c249010000000000000000'],
-            [-18446744073709551616, '3bffffffffffffffff'],
-            [-18446744073709551617, 'c349010000000000000000'],
             [-1, '20'],
             [-10, '29'],
             [-100, '3863'],
@@ -117,36 +112,55 @@ class DecoderTest extends \PHPUnit\Framework\TestCase
             ["a", '6161'],
             ["IETF", '6449455446'],
             ["\"\\", '62225c'],
-            ["\u00fc", '62c3bc'],
-            ["\u6c34", '63e6b0b4'],
-            ["\ud800\udd51", '64f0908591'],
+            ["\u{00fc}", '62c3bc'],
+            ["\u{6c34}", '63e6b0b4'],
+            ["\u{10151}", '64f0908591'],
             [[], '80'],
             [[1, 2, 3], '83010203'],
             [[1, [2, 3], [4, 5]], '8301820203820405'],
-// | [1, 2, 3, 4, 5, 6, 7, 8, 9,  | 0x98190102030405060708090a0b0c0d0e |
-// | 10, 11, 12, 13, 14, 15, 16,  | 0f101112131415161718181819         |
-// | 17, 18, 19, 20, 21, 22, 23,  |                                    |
-// | 24, 25]                      |                                    |
+            [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25],
+                '98190102030405060708090a0b0c0d0e0f101112131415161718181819'],
             [[], 'a0'],
             [[1 => 2, 3 => 4], 'a201020304'],
-            // [{"a": 1, "b": [2, 3]}, 'a26161016162820203'],
-            // [["a", {"b": "c"}], '826161a161626163'],
-// | {"a": "A", "b": "B", "c":    | 0xa5616161416162614261636143616461 |
-// | "C", "d": "D", "e": "E"}     | 4461656145                         |
-            // [(_ h'0102', h'030405'), '5f42010243030405ff'],
+            [["a" => 1, "b" => [2, 3]], 'a26161016162820203'],
+            [["a", ["b" => "c"]], '826161a161626163'],
+            [["a" => "A", "b" => "B", "c" => "C", "d" => "D", "e" => "E"],
+                'a56161614161626142616361436164614461656145'],
+            ["\x01\x02\x03\x04\x05", '5f42010243030405ff'],
             ["streaming", '7f657374726561646d696e67ff'],
             [[], '9fff'],
             [[1, [2, 3], [4, 5]], '9f018202039f0405ffff'],
             [[1, [2, 3], [4, 5]], '9f01820203820405ff'],
             [[1, [2, 3], [4, 5]], '83018202039f0405ff'],
             [[1, [2, 3], [4, 5]], '83019f0203ff820405'],
-// | [_ 1, 2, 3, 4, 5, 6, 7, 8,   | 0x9f0102030405060708090a0b0c0d0e0f |
-// | 9, 10, 11, 12, 13, 14, 15,   | 101112131415161718181819ff         |
-// | 16, 17, 18, 19, 20, 21, 22,  |                                    |
-// | 23, 24, 25]                  |                                    |
+            [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25],
+                '9f0102030405060708090a0b0c0d0e0f101112131415161718181819ff'],
             [["a" => 1, "b" => [2, 3]], 'bf61610161629f0203ffff'],
             [["a", ["b" => "c"]], '826161bf61626163ff'],
             [["Fun" => true, "Amt" => -2], 'bf6346756ef563416d7421ff'],
+        ]);
+    }
+
+    /**
+     * These are numbers in the big yellow box in pack()'s documentation
+     */
+    public function rfcBigintExamples(): array
+    {
+        return array_map(function ($vector) {
+            return [hex2bin($vector[1]), $vector[0]];
+        }, [
+            ['18446744073709551615', '1bffffffffffffffff'],
+            ['-18446744073709551616', '3bffffffffffffffff'],
+        ]);
+    }
+
+    public function rfcTagExamples(): array
+    {
+        return array_map(function ($vector) {
+            return [hex2bin($vector[1]), $vector[0]];
+        }, [
+            [18446744073709551616, 'c249010000000000000000'],
+            [-18446744073709551617, 'c349010000000000000000'],
         ]);
     }
 
