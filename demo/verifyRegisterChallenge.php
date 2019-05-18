febfbffee6985c4864cc1d1263c90b0cd433e562
Eric Stern <eric@ericstern.com>
2019-05-18 13:48:35 -0700

    Use new logic in demo

--- demo/verifyRegisterChallenge.php
+++ demo/verifyRegisterChallenge.php
@@ -24,50 +24,7 @@ $data = json_decode($input, true, 512, JSON_THROW_ON_ERROR);
 
 assert($data['type'] === 'public-key');
 
-// parse response
-$response = new \Firehed\U2F\RegisterResponse();
-
-// attestationObject is a CBOR
-// @see https://w3c.github.io/webauthn/#sctn-attestation (6.4)
-$attestationObject = (new Decoder())->decode(unbyte($data['response']['attestationObject']));
-
-// 6.4.4 general format conformance
-assert(isset($attestationObject['authData']));
-assert(isset($attestationObject['fmt']));
-assert(isset($attestationObject['attStmt']));
-// u2f-specific 8.6 (fmt defines the attStmt body, defined all over the spec,
-// search for "$$attStmtType")
-assert($attestationObject['fmt'] === 'fido-u2f');
-$statement = $attestationObject['attStmt'];
-assert(isset($statement['x5c']) && is_array($statement['x5c']));
-assert(count($statement['x5c']) === 1);
-assert(isset($statement['sig']));
-
-
-$response->setSignature($statement['sig']);
-$response->setAttestationCertificate($statement['x5c'][0]);
-
-// -( Parse Authenticator Data )------
-//
-// FIXME: Somewhere in here, we need to sanity check that $ad['rpIdHash']
-// actually is valid for the server origin
-//
-$ad = WebAuthn\AuthenticatorData::parse($attestationObject['authData']);
-$acd = $ad->getAttestedCredentialData();
-assert($acd !== null);
-$response->setKeyHandle($acd['credentialId']);
-// Maybe check [1] === 2 (kty === signing)
-assert($acd['credentialPublicKey'][3] === -7, 'Not ES256');
-$response->setPublicKey(sprintf(
-    '%s%s%s',
-    "\x04",
-    $acd['credentialPublicKey'][-2],
-    $acd['credentialPublicKey'][-3]
-));
-
-$cdj = unbyte($data['response']['clientDataJSON']);
-$response->setClientData(WebAuthn\ClientData::fromJson($cdj));
-
+$response = WebAuthn\RegistrationResponse::fromDecodedJson($data);
 
 log($response, 'register response');
 
