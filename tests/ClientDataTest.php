45516766a1d73c75dd0acdb589265c47f99cf20b
Eric Stern <eric@slant.co>
2019-06-13 16:33:20 -0700

    Go back to raw JSON to avoid potential confusion around slashes

--- tests/ClientDataTest.php
+++ tests/ClientDataTest.php
@@ -42,13 +42,10 @@ class ClientDataTest extends \PHPUnit\Framework\TestCase
             'Test vector should have been 32 bytes'
         );
 
-        $goodData = [
-            'typ' => 'navigator.id.finishEnrollment',
-            'challenge' => 'PfsWR1Umy2V5Al1Bam2tG0yfPLeJElfwRzzAzkYPgzo',
-            'origin' => 'https://u2f.ericstern.com',
-            'cid_pubkey' => '',
-        ];
-        $goodJson = json_encode($goodData, JSON_UNESCAPED_SLASHES);
+        $goodJson = '{"typ":"navigator.id.finishEnrollment","challenge":"PfsWR'.
+            '1Umy2V5Al1Bam2tG0yfPLeJElfwRzzAzkYPgzo","origin":"https://u2f.eri'.'
+            cstern.com","cid_pubkey":""}';
+
         assert($goodJson !== false);
         $clientData = ClientData::fromJson($goodJson);
         $this->assertTrue(
